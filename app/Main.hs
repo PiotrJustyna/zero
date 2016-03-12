9bca5023ecb7cea32a97bc0c31045b716a39913e
Piotr Justyna <piotr.justyna@gmail.com>
2016-03-12 10:10:27 +0000

    Implemented simple game loop and slightly modified the Travis configuration file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,29 @@
 module Main where
 
 import Player
+import System.IO
 
 main :: IO ()
 main = do
     putStrLn "Welcome to zero!"
-    putStrLn "Starting player:"
-    putStrLn . show . heal . hit $ Player "Piotr" 30
+    putStrLn help
+    gameLoop $ Player "Piotr" 30
+
+gameLoop :: Player -> IO ()
+gameLoop player = do
+    hSetBuffering stdin NoBuffering
+    input <- getChar
+    case input of
+        'h' ->  do
+                putStrLn $ "\n" ++ help
+                gameLoop player
+        'n' ->  do
+                putStrLn $ "\n" ++ (show player)
+                gameLoop player
+        'q' ->  putStrLn "\nBye!"
+        -- otherwise -> putStrLn "Wot"
+
+help =
+    "h - help\n" ++
+    "n - new game\n" ++
+    "q - quit"
