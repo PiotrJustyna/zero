139b209a0ac253c405ef9ea7894f14dc86d7363e
Piotr Justyna <piotr.justyna@gmail.com>
2016-04-23 16:18:18 +0100

    Simplified normals calculation.

--- src/Player.hs
+++ src/Player.hs
@@ -84,19 +84,13 @@ zipModelVerticesAndNormalVertices a b =
             a
             b
 
-calculateNormalsForSurfaceVectors :: (V3 Float)
-    -> (V3 Float)
-    -> (V3 Float)
-    -> (V3 Float)
-    -> (V3 Float)
-    -> (V3 Float)
-    -> [V4 Float]
-calculateNormalsForSurfaceVectors (V3 u1X u1Y u1Z) (V3 u2X u2Y u2Z) (V3 u3X u3Y u3Z) (V3 v1X v1Y v1Z) (V3 v2X v2Y v2Z) (V3 v3X v3Y v3Z) =
-    normal1 : normal2 : normal3 : []
+calculateRawNormalsForPoints :: [(V4 Float, V3 Float)] -> [(V4 Float, V3 Float)]
+calculateRawNormalsForPoints ((V4 x0X x0Y x0Z x0W, x0C) : (V4 x1X x1Y x1Z x1W, x1C) : (V4 x2X x2Y x2Z x2W, x2C) : xs) =
+    normal1 : normal2 : normal3 : (calculateRawNormalsForPoints xs)
     where
-        normal1 = V4 (n1X / 10.0) (n1Y / 10.0) (n1Z / 10.0) 1.0
-        normal2 = V4 (n2X / 10.0) (n2Y / 10.0) (n2Z / 10.0) 1.0
-        normal3 = V4 (n3X / 10.0) (n3Y / 10.0) (n3Z / 10.0) 1.0
+        normal1 = (V4 (n1X / 10.0) (n1Y / 10.0) (n1Z / 10.0) 1.0, white)
+        normal2 = (V4 (n2X / 10.0) (n2Y / 10.0) (n2Z / 10.0) 1.0, white)
+        normal3 = (V4 (n3X / 10.0) (n3Y / 10.0) (n3Z / 10.0) 1.0, white)
 
         n1X = (u1Y * v1Z) - (u1Z * v1Y)
         n1Y = (u1Z * v1X) - (u1X * v1Z)
@@ -109,12 +103,6 @@ calculateNormalsForSurfaceVectors (V3 u1X u1Y u1Z) (V3 u2X u2Y u2Z) (V3 u3X u3Y 
         n3X = (u3Y * v3Z) - (u3Z * v3Y)
         n3Y = (u3Z * v3X) - (u3X * v3Z)
         n3Z = (u3X * v3Y) - (u3Y * v3X)
-
-calculateRawNormalsForPoints :: [(V4 Float, V3 Float)] -> [(V4 Float, V3 Float)]
-calculateRawNormalsForPoints ((V4 x0X x0Y x0Z x0W, x0C) : (V4 x1X x1Y x1Z x1W, x1C) : (V4 x2X x2Y x2Z x2W, x2C) : xs) =
-    (head normals, white) : (normals !! 1, white) : (normals !! 2, white) : (calculateRawNormalsForPoints xs)
-    where
-        normals = calculateNormalsForSurfaceVectors (V3 u1X u1Y u1Z) (V3 u2X u2Y u2Z) (V3 u3X u3Y u3Z) (V3 v1X v1Y v1Z) (V3 v2X v2Y v2Z) (V3 v3X v3Y v3Z)
 
         -- u1 = p2 - p1 or x1 - x0
         -- v1 = p3 - p1 or x2 - x0
@@ -152,48 +140,17 @@ calculateRawNormalsForPoints (x0 : xs) = []
 calculateRawNormalsForPoints [] = []
 
 calculateRawNormalsForLines :: [(V4 Float, V3 Float)] -> [(V4 Float, V3 Float)]
-calculateRawNormalsForLines ((V4 x0X x0Y x0Z x0W, x0C) : x1 : (V4 x2X x2Y x2Z x2W, x2C) : x3 : (V4 x4X x4Y x4Z x4W, x4C) : x5 : xs) =
-    (head normals, white) : (head normals, white) : (normals !! 1, white) : (normals !! 1, white) : (normals !! 2, white) : (normals !! 2, white) : (calculateRawNormalsForLines xs)
+calculateRawNormalsForLines vertices = duplicateVertices $ calculateRawNormalsForPoints $ skimVertices vertices
     where
-        normals = calculateNormalsForSurfaceVectors (V3 u1X u1Y u1Z) (V3 u2X u2Y u2Z) (V3 u3X u3Y u3Z) (V3 v1X v1Y v1Z) (V3 v2X v2Y v2Z) (V3 v3X v3Y v3Z)
-
-        -- u1 = p2 - p1 or x2 - x0
-        -- v1 = p3 - p1 or x4 - x0
-
-        u1X = x2X - x0X
-        u1Y = x2Y - x0Y
-        u1Z = x2Z - x0Z
-        v1X = x4X - x0X
-        v1Y = x4Y - x0Y
-        v1Z = x4Z - x0Z
-
-        -- u2 = p3 - p2 or x4 - x2
-        -- v2 = p1 - p2 or x0 - x2
-
-        u2X = x4X - x2X
-        u2Y = x4Y - x2Y
-        u2Z = x4Z - x2Z
-        v2X = x0X - x2X
-        v2Y = x0Y - x2Y
-        v2Z = x0Z - x2Z
-
-        -- u3 = p1 - p3 or x0 - x4
-        -- v3 = p2 - p3 or x2 - x4
-
-        u3X = x0X - x4X
-        u3Y = x0Y - x4Y
-        u3Z = x0Z - x4Z
-        v3X = x2X - x4X
-        v3Y = x2Y - x4Y
-        v3Z = x2Z - x4Z
-
-        white = V3 1.0 1.0 1.0
-calculateRawNormalsForLines (x0 : x1 : x2 : x3 : x4 : xs) = []
-calculateRawNormalsForLines (x0 : x1 : x2 : x3 : xs) = []
-calculateRawNormalsForLines (x0 : x1 : x2 : xs) = []
-calculateRawNormalsForLines (x0 : x1 : xs) = []
-calculateRawNormalsForLines (x0 : xs) = []
-calculateRawNormalsForLines [] = []
+        duplicateVertices (x : xs) = x : x : duplicateVertices xs 
+        duplicateVertices [] = []
+        skimVertices (x0 : x1 : x2 : x3 : x4 : x5 : xs) = x0 : x2 : x4 : skimVertices xs
+        skimVertices (x0 : x1 : x2 : x3 : x4 : xs) = []
+        skimVertices (x0 : x1 : x2 : x3 : xs) = []
+        skimVertices (x0 : x1 : x2 : xs) = []
+        skimVertices (x0 : x1 : xs) = []
+        skimVertices (x0 : xs) = []
+        skimVertices [] = []
 
 calculateRawNormalsForTriangles :: [(V4 Float, V3 Float)] -> [(V4 Float, V3 Float)]
 calculateRawNormalsForTriangles = calculateRawNormalsForPoints
